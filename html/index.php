bf01cb4a796b6763d44275732ede15640c7ac431
Gallage Peiris <gallage.peiris@dcmail.ca>
2026-01-30 11:33:38 -0500

    Second Commit

---
+++ html/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+echo"<h1> SupportDesk Demo </h1>";
+echo"<p> PHP + Apache containeris running. </p>";
+echo"<p> Next step: add REST endpoints + Composer + PHPUnit. </p>";
+echo"";
+
+//phpinfo();
+
